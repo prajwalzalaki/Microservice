bce5e44be0917c850b06812e3d47e78c2c06e409
Zalaki <prajwal.zalaki@dxc.com>
2022-03-29 12:26:49 +0530

    first commit

---
+++ UserServices/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserServices.Database;
+using UserServices.Database.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        DatabaseContext db;
+        public UserController()
+        {
+            db = new DatabaseContext();
+        }
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return db.Users.ToList();
+        }
+
+        // GET api/<UserService>/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        // POST api/<UserService>
+        [HttpPost]
+        public IActionResult Post([FromBody] User model)
+        {
+            try
+            {
+                db.Users.Add(model);
+                db.SaveChanges();
+
+                return StatusCode(StatusCodes.Status201Created, model);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+
+        // PUT api/<UserService>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserService>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
